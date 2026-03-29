b389091745e9a2f398da3b188aa35d34f7e27c6c
bayyubenjamin <bayyujack@gmail.com>
2026-03-30 05:48:50 +0700

    feat: add helper function in contract

--- src/helper.js
+++ src/helper.js
@@ -1,4 +1,4 @@
-// updated at Sen 30 Mar 2026 04:24:31 WIB
+// updated at Sen 30 Mar 2026 05:48:50 WIB
 // update 1 at Min 29 Mar 2026 20:24:36 WIB
 // update 29 at Sen 30 Mar 2026 00:48:02 WIB
 // update 37 at Sen 30 Mar 2026 01:59:01 WIB
