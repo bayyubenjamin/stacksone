bb4ccda60f76aab4a18efb15a4fbe34f3f39394d
bayyubenjamin <bayyujack@gmail.com>
2026-03-29 22:58:43 +0700

    feat: implement new contract pattern

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-// update 2 at Min 29 Mar 2026 20:16:52 WIB
+// updated at Min 29 Mar 2026 22:58:43 WIB
 // update 2 at Min 29 Mar 2026 20:25:34 WIB
 // update 5 at Min 29 Mar 2026 20:27:03 WIB
 // update 12 at Min 29 Mar 2026 20:31:13 WIB
