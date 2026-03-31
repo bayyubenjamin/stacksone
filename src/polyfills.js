c4f66d921f7c7f6092d528ffcec4df5f38f79b68
bayyubenjamin <bayyujack@gmail.com>
2026-03-31 20:11:00 +0700

    fix: adjust function return

--- src/polyfills.js
+++ src/polyfills.js
@@ -4,3 +4,4 @@ window.global = window;
 window.Buffer = Buffer;
 // update 88 at Sel 31 Mar 2026 15:03:04 WIB
 // update 38 at Sel 31 Mar 2026 17:52:05 WIB
+// update 73 at Sel 31 Mar 2026 20:11:00 WIB
