79a2bffaec11bff2ac725256d6700c8fb5a38c30
bayyubenjamin <bayyujack@gmail.com>
2026-04-01 00:40:07 +0700

    refactor: streamline code

--- src/polyfills.js
+++ src/polyfills.js
@@ -5,3 +5,4 @@ window.Buffer = Buffer;
 // update 88 at Sel 31 Mar 2026 15:03:04 WIB
 // update 38 at Sel 31 Mar 2026 17:52:05 WIB
 // update 73 at Sel 31 Mar 2026 20:11:00 WIB
+// update 137 at Rab 01 Apr 2026 00:40:07 WIB
