7e0e1def30aef357ff8c9b38608e3514a87a797e
bayyubenjamin <bayyujack@gmail.com>
2026-03-31 18:59:49 +0700

    fix: correct variable naming

--- src/main.jsx
+++ src/main.jsx
@@ -16,3 +16,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 // update 26 at Sel 31 Mar 2026 10:57:01 WIB
 // update 66 at Sel 31 Mar 2026 13:34:10 WIB
 // update 76 at Sel 31 Mar 2026 14:14:00 WIB
+// update 55 at Sel 31 Mar 2026 18:59:49 WIB
