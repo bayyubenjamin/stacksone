1422341f2464c7668c75acc81afd360dcb7103a0
bayyubenjamin <bayyujack@gmail.com>
2026-04-04 13:51:48 +0700

    fix: correct variable naming

--- src/polyfills.js
+++ src/polyfills.js
@@ -8,3 +8,4 @@ window.Buffer = Buffer;
 // update 137 at Rab 01 Apr 2026 00:40:07 WIB
 // update 3 at Sab 04 Apr 2026 08:54:13 WIB
 // update 5 at Sab 04 Apr 2026 09:08:07 WIB
+// update 41 at Sab 04 Apr 2026 13:51:48 WIB
