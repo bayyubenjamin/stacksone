54215a93049668a1a77fd9434feba5385128f606
bayyubenjamin <bayyujack@gmail.com>
2026-04-06 06:41:55 +0700

    fix: adjust function return

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -22,3 +22,4 @@ export const userSession = new UserSession({ appConfig });
 // update 39 at Min 05 Apr 2026 17:58:16 WIB
 // update 46 at Min 05 Apr 2026 18:49:24 WIB
 // update 100 at Sen 06 Apr 2026 01:22:58 WIB
+// update 142 at Sen 06 Apr 2026 06:41:55 WIB
