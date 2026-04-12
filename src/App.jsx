fbe2d0eb49f34b1d2c2c31bcb8824aad59daa475
bayyubenjamin <bayyujack@gmail.com>
2026-04-12 19:13:25 +0700

    refactor: restructure components

--- src/App.jsx
+++ src/App.jsx
@@ -380,3 +380,4 @@ export default App;
 // update 16 at Sab 11 Apr 2026 16:07:33 WIB
 // update 33 at Sab 11 Apr 2026 18:13:44 WIB
 // update 4 at Min 12 Apr 2026 15:13:52 WIB
+// update 12 at Min 12 Apr 2026 19:13:25 WIB
