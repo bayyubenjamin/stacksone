adc6870327a4fa77e054ae641027d409ae64d0f2
bayyubenjamin <bayyujack@gmail.com>
2026-04-15 11:49:52 +0700

    feat: enhance leaderboard calculation logic

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -36,3 +36,4 @@ export const userSession = new UserSession({ appConfig });
 // update 90 at Sen 13 Apr 2026 17:55:26 WIB
 // update 20 at Rab 15 Apr 2026 10:46:08 WIB
 // update 29 at Rab 15 Apr 2026 11:44:37 WIB
+// update 30 at Rab 15 Apr 2026 11:49:52 WIB
