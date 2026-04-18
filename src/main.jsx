7b0ebd46839b28b3a901d17d471f7810792f488c
bayyubenjamin <bayyujack@gmail.com>
2026-04-19 00:16:20 +0700

    docs: improve architecture documentation clarity

--- src/main.jsx
+++ src/main.jsx
@@ -65,3 +65,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 // update 59 at Sab 18 Apr 2026 16:04:34 WIB
 // update 61 at Sab 18 Apr 2026 16:15:33 WIB
 // update 88 at Sab 18 Apr 2026 19:09:08 WIB
+// update 135 at Min 19 Apr 2026 00:16:20 WIB
