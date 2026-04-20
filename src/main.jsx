333fd97513798ed90241d066ef4e1686cb4a3679
bayyubenjamin <bayyujack@gmail.com>
2026-04-20 09:00:38 +0700

    chore: remove unused helper functions

--- src/main.jsx
+++ src/main.jsx
@@ -75,3 +75,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 // update 74 at Sen 20 Apr 2026 00:37:16 WIB
 // update 121 at Sen 20 Apr 2026 05:38:54 WIB
 // update 122 at Sen 20 Apr 2026 05:44:46 WIB
+// update 153 at Sen 20 Apr 2026 09:00:38 WIB
