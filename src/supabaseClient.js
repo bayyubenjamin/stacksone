3d4e5143f571f38d81330e51345c8e93480e08e7
bayyubenjamin <bayyujack@gmail.com>
2026-04-23 15:48:09 +0700

    docs: improve architecture documentation clarity

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -84,3 +84,4 @@ export const userSession = new UserSession({ appConfig });
 // update 89 at Kam 23 Apr 2026 08:39:49 WIB
 // update 1 at Kam 23 Apr 2026 08:44:44 WIB
 // update 18 at Kam 23 Apr 2026 10:06:33 WIB
+// update 31 at Kam 23 Apr 2026 15:48:09 WIB
