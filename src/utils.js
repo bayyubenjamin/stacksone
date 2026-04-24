b0545911ee719c7e4026834c38c1d528c5ebe299
bayyubenjamin <bayyujack@gmail.com>
2026-04-24 18:14:37 +0700

    fix: handle edge case scenario

--- src/utils.js
+++ src/utils.js
@@ -106,3 +106,4 @@
 // update 33 at Jum 24 Apr 2026 15:00:51 WIB
 // update 35 at Jum 24 Apr 2026 15:13:49 WIB
 // update 5 at Jum 24 Apr 2026 16:01:08 WIB
+// update 33 at Jum 24 Apr 2026 18:14:37 WIB
