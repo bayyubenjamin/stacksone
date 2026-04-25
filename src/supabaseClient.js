656ece492698ab27f872957448bd38798ee49f14
bayyubenjamin <bayyujack@gmail.com>
2026-04-25 17:33:23 +0700

    feat: implement contract utility abstraction

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -97,3 +97,4 @@ export const userSession = new UserSession({ appConfig });
 // update 24 at Sab 25 Apr 2026 12:37:50 WIB
 // update 59 at Sab 25 Apr 2026 15:33:31 WIB
 // update 75 at Sab 25 Apr 2026 16:56:45 WIB
+// update 82 at Sab 25 Apr 2026 17:33:23 WIB
