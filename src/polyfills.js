85fd83728d33f295bf0dfe4d94d44d35e08d7817
bayyubenjamin <bayyujack@gmail.com>
2026-04-28 04:44:46 +0700

    fix: resolve variable scoping issue

--- src/polyfills.js
+++ src/polyfills.js
@@ -87,3 +87,4 @@ window.Buffer = Buffer;
 // update 170 at Min 26 Apr 2026 00:59:38 WIB
 // update 245 at Min 26 Apr 2026 07:10:54 WIB
 // update 35 at Sel 28 Apr 2026 03:06:12 WIB
+// update 54 at Sel 28 Apr 2026 04:44:46 WIB
