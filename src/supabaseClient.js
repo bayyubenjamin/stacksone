c5c6ba180717d42a461d606b79d73951288291a6
bayyubenjamin <bayyujack@gmail.com>
2026-04-28 12:40:10 +0700

    feat: introduce modular contract structure

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -107,3 +107,4 @@ export const userSession = new UserSession({ appConfig });
 // update 135 at Sen 27 Apr 2026 23:47:10 WIB
 // update 37 at Sel 28 Apr 2026 03:17:36 WIB
 // update 41 at Sel 28 Apr 2026 03:40:18 WIB
+// update 148 at Sel 28 Apr 2026 12:40:09 WIB
