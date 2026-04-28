644550b09a9432b8d3959dd7ab0ee88ad0599a9f
bayyubenjamin <bayyujack@gmail.com>
2026-04-28 23:29:12 +0700

    fix: handle edge case scenario

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -112,3 +112,4 @@ export const userSession = new UserSession({ appConfig });
 // update 162 at Sel 28 Apr 2026 13:58:43 WIB
 // update 244 at Sel 28 Apr 2026 20:59:06 WIB
 // update 267 at Sel 28 Apr 2026 22:52:17 WIB
+// update 275 at Sel 28 Apr 2026 23:29:12 WIB
