a571f8673df844ad9296cbd9c9644f699b1e1b8a
bayyubenjamin <bayyujack@gmail.com>
2026-05-01 05:25:25 +0700

    docs: update roadmap milestones

--- src/App.jsx
+++ src/App.jsx
@@ -470,3 +470,4 @@ export default App;
 // update 220 at Kam 30 Apr 2026 20:01:26 WIB
 // update 10 at Jum 01 Mei 2026 03:06:39 WIB
 // update 20 at Jum 01 Mei 2026 04:02:40 WIB
+// update 35 at Jum 01 Mei 2026 05:25:25 WIB
