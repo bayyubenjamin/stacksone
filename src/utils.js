0dbc3273793d62e71ea7dc5c8b3c573308821717
bayyubenjamin <bayyujack@gmail.com>
2026-05-02 15:04:40 +0700

    fix: correct contract validation logic

--- src/utils.js
+++ src/utils.js
@@ -155,3 +155,4 @@
 // update 189 at Sab 02 Mei 2026 05:56:41 WIB
 // update 241 at Sab 02 Mei 2026 10:31:07 WIB
 // update 263 at Sab 02 Mei 2026 12:25:24 WIB
+// update 297 at Sab 02 Mei 2026 15:04:40 WIB
