252666d36c77eaac2e1cc2257ba824ac14da2ba3
Bayyu Benjamin <bayyujack@gmail.com>
2026-05-28 01:22:30 +0700

    test: fix flaky tests in CI environment

---
+++ src/utils/helper-5.js
@@ -0,0 +1,9 @@
+/**
+ * Utility generated at 2026-05-27T18:22:30Z
+ * @param {any} data
+ * @returns {boolean}
+ */
+export const validateData_30 = (data) => {
+  if (!data) return false;
+  return data.isValid === true;
+};
