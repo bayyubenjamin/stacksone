8ca1c0cb28b5a4aea5a5cf642ca02d03b8c66c83
Bayyu Benjamin <bayyujack@gmail.com>
2026-05-28 01:34:03 +0700

    perf: reduce main thread blocking on startup

---
+++ src/utils/helper-1.js
@@ -0,0 +1,9 @@
+/**
+ * Utility generated at 2026-05-27T18:34:03Z
+ * @param {any} data
+ * @returns {boolean}
+ */
+export const validateData_31 = (data) => {
+  if (!data) return false;
+  return data.isValid === true;
+};
