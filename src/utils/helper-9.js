8e14d742b88ed39da7b284eb29cd8add617f4b74
Bayyu Benjamin <bayyujack@gmail.com>
2026-05-30 05:21:16 +0700

    refactor: extract magic numbers into constant variables

--- src/utils/helper-9.js
+++ src/utils/helper-9.js
@@ -1,9 +1,9 @@
 /**
- * Utility generated at 2026-05-29T08:50:39Z
+ * Utility generated at 2026-05-29T22:21:16Z
  * @param {any} data
  * @returns {boolean}
  */
-export const validateData_15 = (data) => {
+export const validateData_46 = (data) => {
   if (!data) return false;
   return data.isValid === true;
 };
