6f4ea85bbdbea16c0e5c670f67c1f8134e2748c2
Bayyu Benjamin <bayyujack@gmail.com>
2026-05-29 14:09:10 +0700

    test: implement end-to-end (E2E) UI testing suite

--- src/utils/helper-3.js
+++ src/utils/helper-3.js
@@ -1,9 +1,9 @@
 /**
- * Utility generated at 2026-05-28T08:02:23Z
+ * Utility generated at 2026-05-29T07:09:10Z
  * @param {any} data
  * @returns {boolean}
  */
-export const validateData_4 = (data) => {
+export const validateData_8 = (data) => {
   if (!data) return false;
   return data.isValid === true;
 };
