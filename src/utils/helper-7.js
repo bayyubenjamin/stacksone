f0334b8eb8815781c89c21d334c16b8015ae0b28
Bayyu Benjamin <bayyujack@gmail.com>
2026-05-31 06:11:54 +0700

    refactor: abstract complex SQL queries into ORM methods

--- src/utils/helper-7.js
+++ src/utils/helper-7.js
@@ -1,9 +1,9 @@
 /**
- * Utility generated at 2026-05-30T04:12:55Z
+ * Utility generated at 2026-05-30T23:11:54Z
  * @param {any} data
  * @returns {boolean}
  */
-export const validateData_69 = (data) => {
+export const validateData_65 = (data) => {
   if (!data) return false;
   return data.isValid === true;
 };
