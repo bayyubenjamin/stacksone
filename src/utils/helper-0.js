207c1da04e213814cd680c20baf8fc461f198bd6
Bayyu Benjamin <bayyujack@gmail.com>
2026-05-31 08:13:33 +0700

    refactor: reorganize file directory structure

--- src/utils/helper-0.js
+++ src/utils/helper-0.js
@@ -1,9 +1,9 @@
 /**
- * Utility generated at 2026-05-30T23:23:19Z
+ * Utility generated at 2026-05-31T01:13:33Z
  * @param {any} data
  * @returns {boolean}
  */
-export const validateData_66 = (data) => {
+export const validateData_73 = (data) => {
   if (!data) return false;
   return data.isValid === true;
 };
