e01acb0f548042d7e58b5774c8ca9f7f9819b769
Bayyu Benjamin <bayyujack@gmail.com>
2026-06-09 17:04:58 +0700

    chore: update third-party SDK integrations

--- src/utils/helper-7.js
+++ src/utils/helper-7.js
@@ -1,9 +1,9 @@
 /**
- * Utility generated at 2026-05-30T23:11:54Z
+ * Utility generated at 2026-06-09T10:04:58Z
  * @param {any} data
  * @returns {boolean}
  */
-export const validateData_65 = (data) => {
+export const validateData_22 = (data) => {
   if (!data) return false;
   return data.isValid === true;
 };
