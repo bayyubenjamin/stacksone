057e957349d099bbfe3928d60d00d6688c7fa6f8
Bayyu Benjamin <bayyujack@gmail.com>
2026-06-10 21:01:18 +0700

    feat: add smart contract event listeners

--- src/utils/helper-6.js
+++ src/utils/helper-6.js
@@ -1,9 +1,9 @@
 /**
- * Utility generated at 2026-06-09T06:17:44Z
+ * Utility generated at 2026-06-10T14:01:18Z
  * @param {any} data
  * @returns {boolean}
  */
-export const validateData_6 = (data) => {
+export const validateData_34 = (data) => {
   if (!data) return false;
   return data.isValid === true;
 };
