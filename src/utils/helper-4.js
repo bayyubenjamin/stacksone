c6c62770f691f28442bf123d1bd7cbf386093a83
Bayyu Benjamin <bayyujack@gmail.com>
2026-06-10 17:00:56 +0700

    refactor: extract helper functions into utility modules

--- src/utils/helper-4.js
+++ src/utils/helper-4.js
@@ -1,9 +1,9 @@
 /**
- * Utility generated at 2026-05-30T22:07:38Z
+ * Utility generated at 2026-06-10T10:00:56Z
  * @param {any} data
  * @returns {boolean}
  */
-export const validateData_61 = (data) => {
+export const validateData_18 = (data) => {
   if (!data) return false;
   return data.isValid === true;
 };
