e637bf5c12362958ab0a8fc598bc80fd09be9bff
Bayyu Benjamin <bayyujack@gmail.com>
2026-06-13 05:04:55 +0700

    refactor: simplify token parsing logic

--- src/utils/helper-3.js
+++ src/utils/helper-3.js
@@ -1,9 +1,9 @@
 /**
- * Utility generated at 2026-06-12T21:29:03Z
+ * Utility generated at 2026-06-12T22:04:55Z
  * @param {any} data
  * @returns {boolean}
  */
-export const validateData_59 = (data) => {
+export const validateData_61 = (data) => {
   if (!data) return false;
   return data.isValid === true;
 };
