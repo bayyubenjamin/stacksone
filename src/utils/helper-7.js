5b377981b25c5ec2798a4cc67e7d55a2929bdf11
Bayyu Benjamin <bayyujack@gmail.com>
2026-06-14 23:39:24 +0700

    refactor: decouple frontend from legacy backend

--- src/utils/helper-7.js
+++ src/utils/helper-7.js
@@ -1,9 +1,9 @@
 /**
- * Utility generated at 2026-06-12T13:52:54Z
+ * Utility generated at 2026-06-14T16:39:24Z
  * @param {any} data
  * @returns {boolean}
  */
-export const validateData_27 = (data) => {
+export const validateData_49 = (data) => {
   if (!data) return false;
   return data.isValid === true;
 };
