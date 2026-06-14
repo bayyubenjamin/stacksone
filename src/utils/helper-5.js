396563e269028e24646d937a26f23637d910a3a3
Bayyu Benjamin <bayyujack@gmail.com>
2026-06-14 19:37:35 +0700

    test: mock external APIs for isolated testing

--- src/utils/helper-5.js
+++ src/utils/helper-5.js
@@ -1,9 +1,9 @@
 /**
- * Utility generated at 2026-06-13T03:50:56Z
+ * Utility generated at 2026-06-14T12:37:35Z
  * @param {any} data
  * @returns {boolean}
  */
-export const validateData_83 = (data) => {
+export const validateData_33 = (data) => {
   if (!data) return false;
   return data.isValid === true;
 };
