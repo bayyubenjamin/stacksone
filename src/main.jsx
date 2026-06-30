aa573ffb2a08bd15622983767dfbde271e770c02
SMOKAHONTAS <69312778+bayyubenjamin@users.noreply.github.com>
2026-06-30 11:30:47 +0700

    refactor: clean application bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,160 +1,21 @@
-import './polyfills'; // <--- WAJIB DI BARIS 1
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'; // Tambahkan ini
+import './polyfills';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import App from './App.jsx';
+import './index.css';
+
+const rootElement = document.getElementById('root');
+
+if (!rootElement) {
+  throw new Error('Root element #root was not found');
+}
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
-)
-// update 2 at Sel 31 Mar 2026 09:22:34 WIB
-// update 26 at Sel 31 Mar 2026 10:57:01 WIB
-// update 66 at Sel 31 Mar 2026 13:34:10 WIB
-// update 76 at Sel 31 Mar 2026 14:14:00 WIB
-// update 55 at Sel 31 Mar 2026 18:59:49 WIB
-// update 72 at Sel 31 Mar 2026 20:07:58 WIB
-// update 105 at Sel 31 Mar 2026 22:22:35 WIB
-// update 140 at Rab 01 Apr 2026 00:53:07 WIB
-// update 12 at Sab 04 Apr 2026 10:06:46 WIB
-// update 30 at Sab 04 Apr 2026 12:30:29 WIB
-// update 34 at Sab 04 Apr 2026 12:55:48 WIB
-// update 72 at Sab 04 Apr 2026 17:39:19 WIB
-// update 101 at Sab 04 Apr 2026 21:34:38 WIB
-// update 111 at Sab 04 Apr 2026 22:47:21 WIB
-// update 113 at Sab 04 Apr 2026 23:00:45 WIB
-// update 128 at Min 05 Apr 2026 00:56:00 WIB
-// update 149 at Min 05 Apr 2026 03:34:52 WIB
-// update 60 at Min 05 Apr 2026 20:34:01 WIB
-// update 133 at Sen 06 Apr 2026 05:26:40 WIB
-// update 150 at Sen 06 Apr 2026 07:47:23 WIB
-// update 33 at Sen 06 Apr 2026 22:07:31 WIB
-// update 45 at Sen 06 Apr 2026 23:47:12 WIB
-// update 64 at Sel 07 Apr 2026 02:03:49 WIB
-// update 89 at Sel 07 Apr 2026 05:11:38 WIB
-// update 96 at Sel 07 Apr 2026 05:55:31 WIB
-// update 14 at Rab 08 Apr 2026 12:28:13 WIB
-// update 34 at Rab 08 Apr 2026 14:56:19 WIB
-// update 70 at Rab 08 Apr 2026 19:21:57 WIB
-// update 15 at Kam 09 Apr 2026 10:31:30 WIB
-// update 26 at Jum 10 Apr 2026 14:28:55 WIB
-// update 8 at Sab 11 Apr 2026 15:06:06 WIB
-// update 60 at Sab 11 Apr 2026 21:35:27 WIB
-// update 105 at Min 12 Apr 2026 03:27:33 WIB
-// update 111 at Min 12 Apr 2026 04:12:44 WIB
-// update 135 at Min 12 Apr 2026 07:15:48 WIB
-// update 144 at Min 12 Apr 2026 08:24:09 WIB
-// update 150 at Min 12 Apr 2026 09:16:37 WIB
-// update 11 at Sen 13 Apr 2026 10:04:13 WIB
-// update 12 at Sen 13 Apr 2026 10:11:39 WIB
-// update 48 at Sen 13 Apr 2026 13:48:39 WIB
-// update 88 at Sen 13 Apr 2026 17:44:27 WIB
-// update 45 at Kam 16 Apr 2026 03:28:12 WIB
-// update 70 at Kam 16 Apr 2026 06:01:44 WIB
-// update 101 at Kam 16 Apr 2026 09:00:53 WIB
-// update 13 at Kam 16 Apr 2026 19:34:03 WIB
-// update 113 at Jum 17 Apr 2026 05:42:04 WIB
-// update 124 at Jum 17 Apr 2026 06:50:39 WIB
-// update 8 at Sab 18 Apr 2026 10:27:05 WIB
-// update 32 at Sab 18 Apr 2026 13:03:10 WIB
-// update 51 at Sab 18 Apr 2026 15:11:02 WIB
-// update 59 at Sab 18 Apr 2026 16:04:34 WIB
-// update 61 at Sab 18 Apr 2026 16:15:33 WIB
-// update 88 at Sab 18 Apr 2026 19:09:08 WIB
-// update 135 at Min 19 Apr 2026 00:16:20 WIB
-// update 207 at Min 19 Apr 2026 07:54:20 WIB
-// update 215 at Min 19 Apr 2026 08:46:20 WIB
-// update 225 at Min 19 Apr 2026 09:46:25 WIB
-// update 255 at Min 19 Apr 2026 12:51:41 WIB
-// update 25 at Min 19 Apr 2026 19:08:20 WIB
-// update 66 at Min 19 Apr 2026 23:48:15 WIB
-// update 74 at Sen 20 Apr 2026 00:37:16 WIB
-// update 121 at Sen 20 Apr 2026 05:38:54 WIB
-// update 122 at Sen 20 Apr 2026 05:44:46 WIB
-// update 153 at Sen 20 Apr 2026 09:00:38 WIB
-// update 157 at Sen 20 Apr 2026 09:25:01 WIB
-// update 192 at Sen 20 Apr 2026 13:02:08 WIB
-// update 2 at Sen 20 Apr 2026 14:40:52 WIB
-// update 8 at Sen 20 Apr 2026 15:22:54 WIB
-// update 34 at Sen 20 Apr 2026 18:13:59 WIB
-// update 46 at Sen 20 Apr 2026 19:39:16 WIB
-// update 70 at Sen 20 Apr 2026 22:25:35 WIB
-// update 170 at Sel 21 Apr 2026 08:49:28 WIB
-// update 177 at Sel 21 Apr 2026 09:38:41 WIB
-// update 12 at Sel 21 Apr 2026 14:48:38 WIB
-// update 17 at Sel 21 Apr 2026 15:20:30 WIB
-// update 32 at Sel 21 Apr 2026 16:55:32 WIB
-// update 75 at Sel 21 Apr 2026 21:21:15 WIB
-// update 107 at Rab 22 Apr 2026 00:56:49 WIB
-// update 135 at Rab 22 Apr 2026 04:06:04 WIB
-// update 140 at Rab 22 Apr 2026 04:42:56 WIB
-// update 161 at Rab 22 Apr 2026 06:59:16 WIB
-// update 173 at Rab 22 Apr 2026 08:14:10 WIB
-// update 175 at Rab 22 Apr 2026 08:27:52 WIB
-// update 63 at Rab 22 Apr 2026 21:26:27 WIB
-// update 26 at Kam 23 Apr 2026 01:51:44 WIB
-// update 35 at Kam 23 Apr 2026 02:46:39 WIB
-// update 60 at Kam 23 Apr 2026 05:28:56 WIB
-// update 24 at Kam 23 Apr 2026 10:33:34 WIB
-// update 45 at Kam 23 Apr 2026 12:13:00 WIB
-// update 2 at Kam 23 Apr 2026 13:11:10 WIB
-// update 25 at Kam 23 Apr 2026 15:17:28 WIB
-// update 32 at Kam 23 Apr 2026 15:52:33 WIB
-// update 35 at Kam 23 Apr 2026 16:09:16 WIB
-// update 66 at Kam 23 Apr 2026 18:47:24 WIB
-// update 156 at Jum 24 Apr 2026 02:10:17 WIB
-// update 206 at Jum 24 Apr 2026 06:29:41 WIB
-// update 72 at Jum 24 Apr 2026 21:50:31 WIB
-// update 76 at Jum 24 Apr 2026 22:07:03 WIB
-// update 97 at Jum 24 Apr 2026 23:52:38 WIB
-// update 143 at Sab 25 Apr 2026 03:40:53 WIB
-// update 148 at Sab 25 Apr 2026 04:05:43 WIB
-// update 155 at Sab 25 Apr 2026 04:46:46 WIB
-// update 169 at Sab 25 Apr 2026 05:55:10 WIB
-// update 193 at Sab 25 Apr 2026 08:00:30 WIB
-// update 50 at Sab 25 Apr 2026 14:48:01 WIB
-// update 182 at Min 26 Apr 2026 02:03:11 WIB
-// update 184 at Min 26 Apr 2026 02:11:38 WIB
-// update 12 at Sen 27 Apr 2026 13:32:12 WIB
-// update 55 at Sen 27 Apr 2026 17:04:43 WIB
-// update 70 at Sen 27 Apr 2026 18:21:18 WIB
-// update 92 at Sen 27 Apr 2026 20:17:08 WIB
-// update 137 at Sen 27 Apr 2026 23:58:17 WIB
-// update 34 at Sel 28 Apr 2026 03:01:32 WIB
-// update 60 at Sel 28 Apr 2026 05:13:27 WIB
-// update 82 at Sel 28 Apr 2026 07:01:33 WIB
-// update 144 at Sel 28 Apr 2026 12:19:11 WIB
-// update 213 at Sel 28 Apr 2026 18:16:59 WIB
-// update 278 at Sel 28 Apr 2026 23:45:05 WIB
-// update 285 at Rab 29 Apr 2026 00:23:26 WIB
-// update 291 at Rab 29 Apr 2026 00:51:38 WIB
-// update 11 at Rab 29 Apr 2026 10:45:07 WIB
-// update 8 at Rab 29 Apr 2026 13:47:45 WIB
-// update 13 at Rab 29 Apr 2026 14:14:47 WIB
-// update 27 at Rab 29 Apr 2026 15:27:35 WIB
-// update 6 at Rab 29 Apr 2026 23:56:11 WIB
-// update 91 at Kam 30 Apr 2026 09:10:31 WIB
-// update 163 at Kam 30 Apr 2026 15:12:11 WIB
-// update 193 at Kam 30 Apr 2026 17:39:05 WIB
-// update 59 at Jum 01 Mei 2026 07:22:19 WIB
-// update 83 at Jum 01 Mei 2026 09:27:47 WIB
-// update 102 at Jum 01 Mei 2026 11:07:28 WIB
-// update 13 at Jum 01 Mei 2026 15:02:04 WIB
-// update 73 at Jum 01 Mei 2026 20:17:16 WIB
-// update 182 at Sab 02 Mei 2026 05:21:30 WIB
-// update 188 at Sab 02 Mei 2026 05:51:08 WIB
-// update 227 at Sab 02 Mei 2026 09:20:34 WIB
-// update 240 at Sab 02 Mei 2026 10:25:50 WIB
-// update 286 at Sab 02 Mei 2026 14:11:38 WIB
-// update 6 at Min 03 Mei 2026 17:45:08 WIB
-// update 30 at Min 03 Mei 2026 19:44:08 WIB
-// update 32 at Min 03 Mei 2026 19:53:33 WIB
-// update 36 at Min 03 Mei 2026 20:15:11 WIB
-// update 67 at Min 03 Mei 2026 22:56:47 WIB
-// update 93 at Sen 04 Mei 2026 01:03:11 WIB
-// update 118 at Sen 04 Mei 2026 03:02:36 WIB
-// update 165 at Sen 04 Mei 2026 07:01:31 WIB
+);
